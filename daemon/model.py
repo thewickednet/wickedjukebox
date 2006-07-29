42a33716b830a7dc4e45cba30d20c75782422177
Michel Albert <michel@albert.lu>
2006-07-29 11:54:41 +0000

    Completed the database model
    !! The 'duration' field for songs is not yet filled in !! (int or time?)
    
    
    
    git-svn-id: http://svn.foobar.lu/wickedjukebox/trunk@32 f24607ba-0e10-0410-b95a-ef2f3848dab9

--- daemon/model.py
+++ daemon/model.py
@@ -1,7 +1,7 @@
 from sqlobject import *
 
 import ConfigParser
-import string
+import string, datetime
 
 def LoadConfig(file, config={}):
     """
@@ -39,10 +39,12 @@ class Songs(SQLObject):
    class sqlmeta:
       idName = 'song_id'
 
-   artist     = ForeignKey('artists')
+   albums     = RelatedJoin('Albums', intermediateTable='album_song', joinColumn='song_id', otherColumn='album_id')
+
+   artist     = MultipleJoin('Artists', joinColumn='artist_id')
    title      = StringCol(length=128)
-   ##duration   time     No    00:00:00
-   genre_id   = IntCol()
+   #duration   time     No    00:00:00
+   genre      = MultipleJoin('Genres', joinColumn='genre_id')
    year       = StringCol(length=4)
    localpath  = StringCol()
    played     = IntCol()
@@ -56,6 +58,78 @@ class Songs(SQLObject):
    lyrics     = StringCol()
    dirty      = BoolCol()
 
+class Albums(SQLObject):
+
+   class sqlmeta:
+      idName = 'album_id'
+
+   songs   = RelatedJoin('Songs', intermediateTable='album_song', joinColumn='album_id', otherColumn='song_id')
+   title   = StringCol(length=128)
+   added   = DateTimeCol()
+   artists = MultipleJoin('Artists', joinColumn='artist_id')
+
+class Channels(SQLObject):
+
+   class sqlmeta:
+      idName = 'channel_id'
+
+   name           = StringCol(length=32)
+   public         = BoolCol()
+   backend        = StringCol(length=32)
+   backend_params = StringCol()
+
+class Genres(SQLObject):
+
+   class sqlmeta:
+      idName = 'genre_id'
+
+   name = StringCol(length=64)
+
+class Groups(SQLObject):
+
+   class sqlmeta:
+      idName = 'group_id'
+
+   title         = StringCol(length=32)
+   admin         = BoolCol()
+   nocredits     = BoolCol()
+   queue_skip    = BoolCol()
+   queue_remove  = BoolCol()
+
+class Playlist(SQLObject):
+
+   class sqlmeta:
+      idName = 'playlist_id'
+
+   title    = StringCol(length=64)
+   user     = MultipleJoin('Users', joinColumn='user_id')
+   public   = BoolCol()
+   added    = DateTimeCol()
+
+class Queue(SQLObject):
+
+   class sqlmeta:
+      idName = 'queue_id'
+
+   songs      = MultipleJoin('Songs', joinColumn='song_id')
+   users      = MultipleJoin('Users', joinColumn='user_id')
+   channel    = MultipleJoin('Channels', joinColumn='channel_id')
+   position   = IntCol()
+   added      = DateTimeCol()
+
+class Users(SQLObject):
+
+   class sqlmeta:
+      idName = 'user_id'
+
+   username    = StringCol(length=32)
+   password    = StringCol(length=32)
+   fullname    = StringCol(length=64)
+   added       = DateTimeCol()
+   credits     = IntCol()
+
+# ----------------------------------------------------------------------------
+
 dburi = "%s://%s:%s@%s/%s" % (
       config['database.dbms'],
       config['database.user'],
@@ -65,3 +139,4 @@ dburi = "%s://%s:%s@%s/%s" % (
       )
 
 sqlhub.processConnection = connectionForURI(dburi)
+
