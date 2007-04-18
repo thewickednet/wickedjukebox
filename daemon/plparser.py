186dcf86b7b7877a8e9ae3c0288aa6c3d77eba13
Michel Albert <michel@albert.lu>
2007-04-18 17:32:10 +0000

    Actually,... it's a parser.
    git-svn-id: http://svn.foobar.lu/wickedjukebox/trunk@162 f24607ba-0e10-0410-b95a-ef2f3848dab9

---
+++ daemon/plparser.py
@@ -0,0 +1,112 @@
+"""
+Methods to convert a simplified filter string into an SQL query
+"""
+import ply.lex as lex
+import ply.yacc as yacc
+
+# list of tokens
+tokens = (
+   'FIELD',
+   'AND',
+   'OR',
+   'NOT',
+   'EQUALS',
+   'LIKE',
+   'VALUE',
+   'LPAREN',
+   'RPAREN',
+   'QUOTE'
+   )
+
+# reserved words
+reserved = {
+   'and'       : 'AND',
+   'or'        : 'OR',
+   'is'        : 'EQUALS',
+   'contains'  : 'LIKE',
+   'artist'    : 'FIELD',
+   'title'     : 'FIELD',
+   'genre'     : 'FIELD',
+   'dirty'     : 'FIELD',
+   }
+
+# regexps
+t_AND     = r'&'
+t_OR      = r'\|'
+t_EQUALS  = r'='
+t_LIKE    = r'~'
+t_LPAREN  = r'\(|\[|\{'
+t_RPAREN  = r'\)|\]|\}'
+t_QUOTE   = r'"|\''
+t_ignore  =  ' \t\n'
+def t_error(t):
+   print "Illegal character '%s'" % t.value[0]
+   t.lexer.skip(1)
+
+def t_value(t):
+   r'["\'].*["\']'
+   t.type  = 'VALUE'
+   t.value = t.value[1:-1]
+   return t
+
+def t_ID(t):
+   r'\w+'
+   t.type = reserved.get(t.value, 'VALUE')
+   return t
+
+lex.lex()
+
+# ----------------------------------------------------------------------
+
+def p_statement(p):
+   '''statement : expression
+                | statement OR expression
+                | statement AND expression
+                | LPAREN statement RPAREN'''
+   if len(p) == 2:
+      p[0] = p[1]
+   elif len(p) == 4 and p[2] in ['and', '&']:
+      p[0] = "%s AND %s" % (p[1], p[3])
+   elif len(p) == 4 and p[2] in ['or', '|']:
+      p[0] = "%s OR %s" % (p[1], p[3])
+   elif len(p) == 4 and p[1] in ['(', '{', '[']:
+      p[0] = "(%s)" % p[2]
+   return p[0]
+
+def p_expression(p):
+   '''expression : FIELD EQUALS VALUE
+                | FIELD LIKE VALUE'''
+   if p[2] in ['is', '=']:
+      p[0] = "%s = '%s'" % (p[1], p[3])
+   elif p[2] in ['contains', '~']:
+      p[0] = "%s LIKE '%%%s%%'" % (p[1], p[3])
+
+def p_error(t):
+   print "Syntax error at '%s'" % t.value
+
+yacc.yacc()
+
+# ----------------------------------------------------------------------
+
+def getTokens(data):
+   lex.input(data)
+   while(1):
+      tok = lex.token()
+      if not tok: break
+      print tok
+
+def doParse(data):
+   return yacc.parse(data)
+
+if __name__ == "__main__":
+   testinput = [
+      '(genre = rock | genre = "Heavy Metal" or genre = Industrial) & title ~ wicked',
+      'artist is "Nine Inch Nails" or artist is "Black Sabbath" or artist is Clawfinger or album contains "Nativity in Black" or artist is Incubus'
+   ]
+   for line in testinput:
+      print 80*"="
+      print 'Simplified query: "%s"' % line.strip()
+      print 80*"-"
+      print "result:", doParse(line)
+      print
+
