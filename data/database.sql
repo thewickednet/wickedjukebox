49c21e24c08f693b938ecfea6f331ed1b57f4d3f
Michel Albert <michel@albert.lu>
2007-05-27 13:29:06 +0000

    More elaborate database structure
    git-svn-id: http://svn.foobar.lu/wickedjukebox/trunk@169 f24607ba-0e10-0410-b95a-ef2f3848dab9

--- data/database.sql
+++ data/database.sql
@@ -1,6 +1,11 @@
-DROP TABLE setting;
-DROP TABLE channel;
-DROP TABLE playmode;
+DROP TABLE IF EXISTS setting;
+DROP TABLE IF EXISTS channel;
+DROP TABLE IF EXISTS playmode;
+DROP TABLE IF EXISTS artist;
+DROP TABLE IF EXISTS album;
+DROP TABLE IF EXISTS genre;
+DROP TABLE IF EXISTS song;
+DROP TABLE IF EXISTS channel_song_data;
 
 ----------------------------------------------------------------------------
 
@@ -16,7 +21,11 @@ CREATE TABLE channel(
    id INTEGER NOT NULL PRIMARY KEY,
    name VARCHAR(32) NOT NULL
       CONSTRAINT unique_channel UNIQUE,
+   public INTEGER(1) NOT NULL DEFAULT 1,
    backend VARCHAR(64) NOT NULL,
+   backend_params TEXT,
+   ping DATETIME,
+   active INTEGER(1) NOT NULL DEFAULT 0,
    status INTEGER,
    playmode INTEGER
       REFERENCES playmode(id)
@@ -32,6 +41,89 @@ CREATE TABLE setting(
       REFERENCES channel(id)
          ON UPDATE CASCADE
          ON DELETE RESTRICT
+);
+
+CREATE TABLE artist(
+   id INTEGER NOT NULL PRIMARY KEY,
+   name VARCHAR(128),
+   added DATETIME NOT NULL
+);
+
+CREATE TABLE album(
+   id INTEGER NOT NULL PRIMARY KEY,
+   name VARCHAR(128),
+   added DATETIME NOT NULL,
+   artist_id INTEGER NOT NULL
+      REFERENCES artist(id)
+         ON UPDATE CASCADE
+         ON DELETE RESTRICT,
+   downloaded INTEGER NOT NULL DEFAULT 0,
+   type VARCHAR(32)
+);
+
+CREATE TABLE genre(
+   id INTEGER NOT NULL PRIMARY KEY,
+   name VARCHAR(128),
+   added DATETIME NOT NULL
+);
+
+CREATE TABLE song(
+   id INTEGER NOT NULL PRIMARY KEY,
+   artist_id INTEGER NOT NULL
+      REFERENCES artist(id)
+         ON UPDATE CASCADE
+         ON DELETE RESTRICT,
+   album_id INTEGER NOT NULL
+      REFERENCES album(id)
+         ON UPDATE CASCADE
+         ON DELETE RESTRICT,
+   genre_id INTEGER NOT NULL
+      REFERENCES genre(id)
+         ON UPDATE CASCADE
+         ON DELETE RESTRICT,
+   track_no INTEGER(3) DEFAULT NULL,
+   title VARCHAR(128),
+   duration INTEGER(4),  -- Duration in seconds
+   year INTEGER(4),
+   localpath TEXT NOT NULL,
+   downloaded INTEGER DEFAULT 0,
+   lastScanned DATETIME DEFAULT NULL,
+   bitrate INTEGER(5),
+   filesize INTEGER(32),
+   checksum VARCHAR(14),
+   lyrics TEXT,
+   dirty INTEGER(1) DEFAULT 0,
+   added DATETIME NOT NULL
+);
+
+CREATE TABLE channel_song_data(
+   channel_id INTEGER NOT NULL
+      REFERENCES channel(id)
+         ON UPDATE CASCADE
+         ON DELETE RESTRICT,
+   song_id INTEGER NOT NULL
+      REFERENCES song(id)
+         ON UPDATE CASCADE
+         ON DELETE RESTRICT,
+   played     INTEGER NOT NULL DEFAULT 0,
+   voted      INTEGER NOT NULL DEFAULT 0,
+   skipped    INTEGER NOT NULL DEFAULT 0,
+   lastPlayed DATETIME DEFAULT NULL,
+   cost       INTEGER DEFAULT 5,
+   PRIMARY KEY( channel_id, song_id )
+);
+
+CREATE TABLE channel_album_data(
+   channel_id INTEGER NOT NULL
+      REFERENCES channel(id)
+         ON UPDATE CASCADE
+         ON DELETE RESTRICT,
+   album_id INTEGER NOT NULL
+      REFERENCES album(id)
+         ON UPDATE CASCADE
+         ON DELETE RESTRICT,
+   played     INTEGER NOT NULL DEFAULT 0,
+   PRIMARY KEY( channel_id, album_id )
 );
 
 COMMIT;
