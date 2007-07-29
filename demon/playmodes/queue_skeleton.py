488c64daabfbbcf48bfd62c8a87bf16e82f4a5e2
Michel Albert <michel@albert.lu>
2007-07-29 20:34:09 +0000

    Playmodes documentation
    git-svn-id: http://svn.foobar.lu/wickedjukebox/trunk@238 f24607ba-0e10-0410-b95a-ef2f3848dab9

---
+++ demon/playmodes/queue_skeleton.py
@@ -0,0 +1,30 @@
+"""
+This is a no-op playmode used as blueprint for new playmodes.
+Simply complete the methods in this module and make them return the right type
+and you should be fine.
+"""
+
+#
+# These imports might prove useful:
+#
+## from demon.model import create_session, Song
+## from twisted.python import log
+## from demon.model import create_session, QueueItem
+
+def dequeue():
+   """
+   DESCRIPTION
+      Return the next song from the queue
+
+   PARAMETERS
+      [none]
+
+   RETURNS
+      This should return a valid "Song" instance, which points to the next song
+      on the queue.
+      If nothing was on the queue, or no song could be determnined, then return
+      "None", so the system can fall back to random mode.
+   """
+
+   return None
+
---
+++ demon/playmodes/random_skeleton.py
@@ -0,0 +1,28 @@
+"""
+This is a no-op playmode used as blueprint for new playmodes.
+Simply complete the methods in this module and make them return the right type
+and you should be fine.
+"""
+
+#
+# These imports might prove useful:
+#
+## from demon.model import create_session, Song
+## from twisted.python import log
+
+def get():
+   """
+   DESCRIPTION
+      Returns a random song.
+      NOTE: This method should *always* return a valid Song instance. Only
+            return "None" if something seriously went wrong.
+
+   PARAMETERS
+      [none]
+
+   RETURNS
+      Either "None" if no random song could be determined,
+      or a "Song" instance (as imported from demon.model --> see above)
+   """
+
+   return None
