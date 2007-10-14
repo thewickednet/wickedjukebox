ca32b55c1b8e45ee0d987e0e6797c6c1d4e2d7a5
Michel Albert <michel@albert.lu>
2007-10-14 09:42:12 +0000

    Added settings to simple_control
    git-svn-id: http://svn.foobar.lu/wickedjukebox/trunk@305 f24607ba-0e10-0410-b95a-ef2f3848dab9

---
+++ satellites/simple-control/settings.py
@@ -0,0 +1,3 @@
+channel_id  = 1
+xmlrpc_host = '192.168.1.2'
+xmlrpc_port = 65222
--- satellites/simple-control/simplecon.py
+++ satellites/simple-control/simplecon.py
@@ -2,11 +2,13 @@ import sys, pickle, os
 from PyQt4 import QtCore, QtGui
 from ui_simplecontrol import Ui_wndSimpleController
 import xmlrpclib
+from settings import xmlrpc_host, xmlrpc_port, channel_id
+
 
 class StartQT4(QtGui.QMainWindow):
 
    def __init__(self, parent=None):
-      self.server = xmlrpclib.Server('http://192.168.1.2:61112')
+      self.server = xmlrpclib.Server('http://%s:%d' % (xmlrpc_host, xmlrpc_port))
       QtGui.QWidget.__init__(self, parent)
       self.ui = Ui_wndSimpleController()
       self.ui.setupUi(self)
@@ -16,13 +18,40 @@ class StartQT4(QtGui.QMainWindow):
       QtCore.QObject.connect(self.ui.actionPlay, QtCore.SIGNAL('activated()'), self.play )
 
    def stop(self):
-      print self.server.stop()
+      try:
+         self.ui. self.server.stop(channel_id)
+      except xmlrpclib.Fault:
+         QtGui.QMessageBox.warning(
+               None,
+               'XMLRPC-Fault',
+               'Fault object received when executing "stop"\n' +
+                  'Did you specify the correct channel_id in settings.py?',
+               QtGui.QMessageBox.Ok
+               )
 
    def play(self):
-      print self.server.play()
+      try:
+         print self.server.play(channel_id)
+      except xmlrpclib.Fault:
+         QtGui.QMessageBox.warning(
+               None,
+               'XMLRPC-Fault',
+               'Fault object received when executing "stop"\n' +
+                  'Did you specify the correct channel_id in settings.py?',
+               QtGui.QMessageBox.Ok
+               )
 
    def skip(self):
-      print self.server.next()
+      try:
+         print self.server.next(channel_id)
+      except xmlrpclib.Fault:
+         QtGui.QMessageBox.warning(
+               None,
+               'XMLRPC-Fault',
+               'Fault object received when executing "stop"\n' +
+                  'Did you specify the correct channel_id in settings.py?',
+               QtGui.QMessageBox.Ok
+               )
 
 if __name__ == "__main__":
 
