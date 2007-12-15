1b363751c58552940bdb5c2e2c6a0075ac17fe2f
Michel Albert <michel@albert.lu>
2007-12-15 13:51:38 +0000

    Icons stored in resource file.
    git-svn-id: http://svn.foobar.lu/wickedjukebox/trunk@308 f24607ba-0e10-0410-b95a-ef2f3848dab9

--- satellites/simple-control/ui_simplecontrol.py
+++ satellites/simple-control/ui_simplecontrol.py
@@ -2,25 +2,24 @@
 
 # Form implementation generated from reading ui file 'simpleControl.ui'
 #
-# Created: Tue Aug  7 20:17:01 2007
-#      by: PyQt4 UI code generator 4.1
+# Created: Sat Dec 15 14:51:08 2007
+#      by: PyQt4 UI code generator 4.3
 #
 # WARNING! All changes made in this file will be lost!
 
-import sys
 from PyQt4 import QtCore, QtGui
 
 class Ui_wndSimpleController(object):
     def setupUi(self, wndSimpleController):
         wndSimpleController.setObjectName("wndSimpleController")
-        wndSimpleController.resize(QtCore.QSize(QtCore.QRect(0,0,135,57).size()).expandedTo(wndSimpleController.minimumSizeHint()))
+        wndSimpleController.resize(QtCore.QSize(QtCore.QRect(0,0,140,82).size()).expandedTo(wndSimpleController.minimumSizeHint()))
 
         self.centralwidget = QtGui.QWidget(wndSimpleController)
         self.centralwidget.setObjectName("centralwidget")
 
         self.hboxlayout = QtGui.QHBoxLayout(self.centralwidget)
-        self.hboxlayout.setMargin(9)
         self.hboxlayout.setSpacing(6)
+        self.hboxlayout.setMargin(9)
         self.hboxlayout.setObjectName("hboxlayout")
         wndSimpleController.setCentralWidget(self.centralwidget)
 
@@ -29,16 +28,20 @@ class Ui_wndSimpleController(object):
         self.toolBar.setObjectName("toolBar")
         wndSimpleController.addToolBar(self.toolBar)
 
+        self.statusBar = QtGui.QStatusBar(wndSimpleController)
+        self.statusBar.setObjectName("statusBar")
+        wndSimpleController.setStatusBar(self.statusBar)
+
         self.actionSkip = QtGui.QAction(wndSimpleController)
-        self.actionSkip.setIcon(QtGui.QIcon("icons/player_fwd.png"))
+        self.actionSkip.setIcon(QtGui.QIcon(":/icons/icons/player_fwd.png"))
         self.actionSkip.setObjectName("actionSkip")
 
         self.actionPlay = QtGui.QAction(wndSimpleController)
-        self.actionPlay.setIcon(QtGui.QIcon("icons/player_play.png"))
+        self.actionPlay.setIcon(QtGui.QIcon(":/icons/icons/player_play.png"))
         self.actionPlay.setObjectName("actionPlay")
 
         self.actionStop = QtGui.QAction(wndSimpleController)
-        self.actionStop.setIcon(QtGui.QIcon("icons/player_stop.png"))
+        self.actionStop.setIcon(QtGui.QIcon(":/icons/icons/player_stop.png"))
         self.actionStop.setObjectName("actionStop")
         self.toolBar.addAction(self.actionStop)
         self.toolBar.addAction(self.actionPlay)
@@ -53,3 +56,4 @@ class Ui_wndSimpleController(object):
         self.actionPlay.setText(QtGui.QApplication.translate("wndSimpleController", "play", None, QtGui.QApplication.UnicodeUTF8))
         self.actionStop.setText(QtGui.QApplication.translate("wndSimpleController", "stop", None, QtGui.QApplication.UnicodeUTF8))
 
+import rc_icons_rc
