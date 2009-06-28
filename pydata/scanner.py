2f9041d1401b540c20790c75821e9210d363bb7a
Michel Albert <michel@albert.lu>
2009-06-28 16:18:51 +0000

    More flexible file scanner implemented. Business logic not changed!
    git-svn-id: http://svn.foobar.lu/wickedjukebox/trunk@396 f24607ba-0e10-0410-b95a-ef2f3848dab9

--- pydata/scanner.py
+++ pydata/scanner.py
@@ -9,6 +9,7 @@ from util import fsdecode, fsencode
 import logging
 from demon.model import getSetting, Song, songTable
 from sqlalchemy import create_session, select
+from filescan import scan as fscan
 logger = logging.getLogger(__name__)
 
 valid_extensions = getSetting("recognizedTypes").split(" ")
@@ -42,6 +43,32 @@ def process(localpath, encoding):
    else:
       logger.debug("%r is not a valid audio-file (only scanning extensions %r)" % (localpath, valid_extensions))
 
+def filter_capping( root, filename, capping ):
+   relative_path = filename[len(root)+1:]
+   return not relative_path.startswith(capping)
+
+def filter_valid_extenstion( root, filename ):
+   extinfo = os.path.splitext( filename )
+   if not extinfo:
+      logger.warn("Unable to split extension from file %r. This file will be ignored!" % filename )
+      return True
+   else:
+      extension = extinfo[1].replace(".", "")
+      if extension not in valid_extensions:
+         logger.info("%r is not a valid audio-file (only scanning extensions %r)" % (filename, valid_extensions))
+         return True
+   return False
+
+def processor_todatabase(root, localpath):
+   session = create_session()
+   song = session.query(Song).selectfirst_by( localpath=localpath )
+   if not song:
+      song = Song(localpath, None, None)
+   song.scan_from_file( localpath, encoding )
+   session.save_or_update(song)
+   session.flush()
+   logger.debug( "%r at %r" % (song, localpath) )
+
 def scan(top, capping=u""):
    """
    Scans a folder rootet at <top> for audio files. It will only include files starting with <capping>
@@ -51,14 +78,11 @@ def scan(top, capping=u""):
    """
    logger.info("Starting scan on %r with capping %r" % (top, capping))
 
-   i = 0
    top = fsencode(top)
    capping = fsencode(capping)
-   for root, dirs, files in walk(fsencode(top)):
-      relative_path = root[len(top)+1:]
-      for file in files:
-         if path.join(relative_path, file).startswith(capping):
-            process( *fsdecode( path.join(root, file) ) )
-            i+= 1
-   logger.info( "Scanned %d songs" % i )
+   count = fscan( top,
+          filters=[ (filter_capping, capping), filter_valid_extenstion ],
+          processors = [ processor_todatabase ]
+          )
+   logger.info( "Scanned %d songs" % count )
 
