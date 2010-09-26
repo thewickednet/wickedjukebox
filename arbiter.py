18df4175ce61b500903b090c61faa108b371d2dd
Michel Albert <michel@albert.lu>
2010-09-26 16:15:20 +0200

    New wrapper to detect crashing channels
    The new "arbiter.py" script lauches channels as a controlled separate
    process (relying on Python 2.6's multiprocessing module). If a channel
    dies unexpectedly (exit-status != 0), then the channel is re-started.
    
    NOTE: I had to rename "run-channel.py" to "run_channel.py" to make this
    work as you cannot import names with dashes.

---
+++ arbiter.py
@@ -0,0 +1,42 @@
+from multiprocessing import Process
+import logging
+import logging.config
+from time import sleep
+
+from run_channel import run_channel
+
+logging.config.fileConfig("logging.ini")
+
+LOG = logging.getLogger(__name__)
+POOL =[]
+CHANNELS = ['wicked', 'fiction']
+
+def monitor():
+   while True:
+      for proc in POOL:
+         if not proc.is_alive() and proc.exitcode != 0:
+            LOG.warning( "Channel %-10s died. Restarting...." % proc.name )
+            POOL.remove(proc)
+            new_proc = Process(
+                  target=run_channel,
+                  args=(proc.name,),
+                  name=proc.name )
+            POOL.append( new_proc )
+            new_proc.start()
+      sleep(1)
+
+def main():
+   for channel_name in CHANNELS:
+      proc = Process(
+            target=run_channel,
+            args=(channel_name,),
+            name=channel_name )
+      POOL.append(proc)
+      proc.start()
+
+   # start monitoring
+   monitor()
+
+if __name__ == "__main__":
+   main()
+
