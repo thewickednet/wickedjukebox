d3e3a78c51a0099b8bbefd350dfa91f3ca1509f1
Yves Thommes <ythommes@gmail.com>
2012-06-19 09:44:08 +0200

    defined custom route to handle our REST calls

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -72,5 +72,13 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
         }
     }
 
+    protected function _initRestRoute()
+    {
+        $this->bootstrap('frontController');
+        $frontController = Zend_Controller_Front::getInstance();
+        $restRoute = new Zend_Rest_Route($frontController, array(), array('rest'));
+        $frontController->getRouter()->addRoute('rest', $restRoute);
+    }
+
 }
 
