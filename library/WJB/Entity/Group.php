4892f8fb9fc4fbb07a57eeba139e4003f2f9b11d
Yves Thommes <ythommes@gmail.com>
2012-06-19 22:09:51 +0200

    more Data Entities defined, along with their repositories

---
+++ library/WJB/Entity/Group.php
@@ -0,0 +1,195 @@
+<?php
+
+namespace WJB\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Group
+ *
+ * @ORM\Table(name="groups")
+ * @ORM\Entity(repositoryClass="WJB\Entity\Repository\GroupRepository")
+ */
+class Group
+{
+    /**
+     * @var string $title
+     *
+     * @ORM\Column(name="title", type="string", length=32)
+     */
+    private $title;
+
+    /**
+     * @var integer $admin
+     *
+     * @ORM\Column(name="admin", type="integer")
+     */
+    private $admin;
+
+    /**
+     * @var integer $nocredits
+     *
+     * @ORM\Column(name="nocredits", type="integer")
+     */
+    private $nocredits;
+
+    /**
+     * @var integer $queueSkip
+     *
+     * @ORM\Column(name="queue_skip", type="integer")
+     */
+    private $queueSkip;
+
+    /**
+     * @var integer $queueRemove
+     *
+     * @ORM\Column(name="queue_remove", type="integer")
+     */
+    private $queueRemove;
+
+    /**
+     * @var integer $queueAdd
+     *
+     * @ORM\Column(name="queue_add", type="integer")
+     */
+    private $queueAdd;
+
+    /**
+     * @var integer $id
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @ORM\OneToMany(targetEntity="User", mappedBy="group")
+     * @ORM\OrderBy({"added" = "ASC"})
+     */
+    private $users;
+
+
+    /**
+     * @param int $admin
+     */
+    public function setAdmin($admin)
+    {
+        $this->admin = $admin;
+    }
+
+    /**
+     * @return int
+     */
+    public function getAdmin()
+    {
+        return $this->admin;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $nocredits
+     */
+    public function setNocredits($nocredits)
+    {
+        $this->nocredits = $nocredits;
+    }
+
+    /**
+     * @return int
+     */
+    public function getNocredits()
+    {
+        return $this->nocredits;
+    }
+
+    /**
+     * @param int $queueAdd
+     */
+    public function setQueueAdd($queueAdd)
+    {
+        $this->queueAdd = $queueAdd;
+    }
+
+    /**
+     * @return int
+     */
+    public function getQueueAdd()
+    {
+        return $this->queueAdd;
+    }
+
+    /**
+     * @param int $queueRemove
+     */
+    public function setQueueRemove($queueRemove)
+    {
+        $this->queueRemove = $queueRemove;
+    }
+
+    /**
+     * @return int
+     */
+    public function getQueueRemove()
+    {
+        return $this->queueRemove;
+    }
+
+    /**
+     * @param int $queueSkip
+     */
+    public function setQueueSkip($queueSkip)
+    {
+        $this->queueSkip = $queueSkip;
+    }
+
+    /**
+     * @return int
+     */
+    public function getQueueSkip()
+    {
+        return $this->queueSkip;
+    }
+
+    /**
+     * @param string $title
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    public function setUsers($users)
+    {
+        $this->users = $users;
+    }
+
+    public function getUsers()
+    {
+        return $this->users;
+    }
+}
---
+++ library/WJB/Entity/User.php
@@ -0,0 +1,410 @@
+<?php
+
+namespace WJB\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * User
+ *
+ * @ORM\Table(name="users")
+ * @ORM\Entity(repositoryClass="WJB\Entity\Repository\UserRepository")
+ */
+class User
+{
+    /**
+     * @var string $name
+     *
+     * @ORM\Column(name="name", type="string", length=32)
+     */
+    private $username;
+
+    /**
+     * @var string $password
+     *
+     * @ORM\Column(name="password", type="string", length=32)
+     */
+    private $password;
+
+    /**
+     * @var string $cookie
+     *
+     * @ORM\Column(name="cookie", type="string", length=32)
+     */
+    private $cookie;
+
+    /**
+     * @var string $fullname
+     *
+     * @ORM\Column(name="fullname", type="string", length=64)
+     */
+    private $fullname;
+
+    /**
+     * @var string $name
+     *
+     * @ORM\Column(name="email", type="string", length=128)
+     */
+    private $email;
+
+    /**
+     * @var integer $credits
+     *
+     * @ORM\Column(name="credits", type="integer")
+     */
+    private $credits;
+
+    /**
+     * @var integer $downloads
+     *
+     * @ORM\Column(name="downloads", type="integer")
+     */
+    private $downloads;
+
+    /**
+     * @var integer $votes
+     *
+     * @ORM\Column(name="votes", type="integer")
+     */
+    private $votes;
+
+    /**
+     * @var integer $skips
+     *
+     * @ORM\Column(name="skips", type="integer")
+     */
+    private $skips;
+
+    /**
+     * @var integer $selects
+     *
+     * @ORM\Column(name="selects", type="integer")
+     */
+    private $selects;
+
+    /**
+     * @var string $added
+     *
+     * @ORM\Column(name="added", type="datetime")
+     */
+    private $added;
+
+    /**
+     * @var string $proofOfLife
+     *
+     * @ORM\Column(name="proof_of_life", type="datetime")
+     */
+    private $proofOfLife;
+
+    /**
+     * @var string $proofOfListening
+     *
+     * @ORM\Column(name="proof_of_listening", type="datetime")
+     */
+    private $proofOfListening;
+
+    /**
+     * @var string $picture
+     *
+     * @ORM\Column(name="picture", type="string")
+     */
+    private $picture;
+
+    /**
+     * @var string $proofOfListening
+     *
+     * @ORM\Column(name="IP", type="string")
+     */
+    private $IP;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Group")
+     * @ORM\JoinColumn(name="group_id", referencedColumnName="id")
+     */
+    private $group;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Channel")
+     * @ORM\JoinColumn(name="channel_id", referencedColumnName="id")
+     */
+    private $channel;
+
+    /**
+     * @var integer $id
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+
+    /**
+     * @param string $IP
+     */
+    public function setIP($IP)
+    {
+        $this->IP = $IP;
+    }
+
+    /**
+     * @return string
+     */
+    public function getIP()
+    {
+        return $this->IP;
+    }
+
+    /**
+     * @param string $added
+     */
+    public function setAdded($added)
+    {
+        $this->added = $added;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAdded()
+    {
+        return $this->added;
+    }
+
+    public function setChannel($channel)
+    {
+        $this->channel = $channel;
+    }
+
+    public function getChannel()
+    {
+        return $this->channel;
+    }
+
+    /**
+     * @param string $cookie
+     */
+    public function setCookie($cookie)
+    {
+        $this->cookie = $cookie;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCookie()
+    {
+        return $this->cookie;
+    }
+
+    /**
+     * @param int $credits
+     */
+    public function setCredits($credits)
+    {
+        $this->credits = $credits;
+    }
+
+    /**
+     * @return int
+     */
+    public function getCredits()
+    {
+        return $this->credits;
+    }
+
+    /**
+     * @param int $downloads
+     */
+    public function setDownloads($downloads)
+    {
+        $this->downloads = $downloads;
+    }
+
+    /**
+     * @return int
+     */
+    public function getDownloads()
+    {
+        return $this->downloads;
+    }
+
+    /**
+     * @param string $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string $fullname
+     */
+    public function setFullname($fullname)
+    {
+        $this->fullname = $fullname;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFullname()
+    {
+        return $this->fullname;
+    }
+
+    public function setGroup($group)
+    {
+        $this->group = $group;
+    }
+
+    public function getGroup()
+    {
+        return $this->group;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param string $password
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param string $picture
+     */
+    public function setPicture($picture)
+    {
+        $this->picture = $picture;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPicture()
+    {
+        return $this->picture;
+    }
+
+    /**
+     * @param string $proofOfLife
+     */
+    public function setProofOfLife($proofOfLife)
+    {
+        $this->proofOfLife = $proofOfLife;
+    }
+
+    /**
+     * @return string
+     */
+    public function getProofOfLife()
+    {
+        return $this->proofOfLife;
+    }
+
+    /**
+     * @param string $proofOfListening
+     */
+    public function setProofOfListening($proofOfListening)
+    {
+        $this->proofOfListening = $proofOfListening;
+    }
+
+    /**
+     * @return string
+     */
+    public function getProofOfListening()
+    {
+        return $this->proofOfListening;
+    }
+
+    /**
+     * @param int $selects
+     */
+    public function setSelects($selects)
+    {
+        $this->selects = $selects;
+    }
+
+    /**
+     * @return int
+     */
+    public function getSelects()
+    {
+        return $this->selects;
+    }
+
+    /**
+     * @param int $skips
+     */
+    public function setSkips($skips)
+    {
+        $this->skips = $skips;
+    }
+
+    /**
+     * @return int
+     */
+    public function getSkips()
+    {
+        return $this->skips;
+    }
+
+    /**
+     * @param string $username
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * @param int $votes
+     */
+    public function setVotes($votes)
+    {
+        $this->votes = $votes;
+    }
+
+    /**
+     * @return int
+     */
+    public function getVotes()
+    {
+        return $this->votes;
+    }
+}
