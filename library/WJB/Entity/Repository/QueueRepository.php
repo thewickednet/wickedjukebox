ae328c6f20e920e3a137bf9c17746911ab9f2111
Yves Thommes <ythommes@gmail.com>
2012-07-02 17:36:50 +0200

    if we want to queue a song we'll have to know the last position of the current queue

--- library/WJB/Entity/Repository/QueueRepository.php
+++ library/WJB/Entity/Repository/QueueRepository.php
@@ -12,4 +12,25 @@ use Doctrine\ORM\EntityRepository;
  */
 class QueueRepository extends EntityRepository
 {
+
+
+    public function getLastPosition($channelId)
+    {
+        $em = $this->getEntityManager();
+        $qb = $em->createQueryBuilder();
+
+        $qb->select('q.position')
+            ->from('\WJB\Entity\Queue', 'q')
+            ->where('q.channelId = :channelId')
+            ->orderBy('q.position', 'DESC')
+            ->setMaxResults(1)
+            ->setParameter('channelId', $channelId);
+
+        $query = $qb->getQuery();
+        $result = $query->getSingleResult();
+
+        return $result['position'];
+    }
+
+
 }
