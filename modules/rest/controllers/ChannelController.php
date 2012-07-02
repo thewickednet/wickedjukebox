db08d536588bbdf8b930d3f791c93ecf3548cb76
Yves Thommes <ythommes@gmail.com>
2012-07-02 17:50:22 +0200

    channel rest service will list all available channels along with the current song

--- modules/rest/controllers/ChannelController.php
+++ modules/rest/controllers/ChannelController.php
@@ -1,34 +1,30 @@
 <?php
 
 use WJB\Service\Channel as ChannelService;
+use WJB\Service\Song as SongService;
 
 
 class Rest_ChannelController extends WJB\Rest\Controller
 {
 
-    private $_em;
-
     public function init()
     {
         parent::init();
-        $this->_em = $this->getInvokeArg('bootstrap')->getResource('doctrine')->getEntityManager();
-        $this->_channelRep = $this->_em->getRepository('\WJB\Entity\Channel');
     }
 
     public function indexAction()
     {
 
-        $channelService = new ChannelService();
-
+        $channelService = new \WJB\Service\Channel();
         $channels = $channelService->getAll();
 
+        $results = array();
         foreach ($channels as $channel)
         {
-            $results[] = array(
-                'name' => $channel->getName(),
-                'backend' => $channel->getBackend(),
-                'public' => $channel->getPublic()
-            );
+            if ($channel->getPublic() == 0)
+                continue;
+            $results[] = $channelService->toArray($channel);
+
         }
         $this->view->payload = $results;
         $this->getResponse()
@@ -36,6 +32,64 @@ class Rest_ChannelController extends WJB\Rest\Controller
 
     }
 
+
+    public function getAction()
+    {
+
+        $id = $this->getRequest()->getParam('id');
+
+        $channelService = new ChannelService();
+        $channel = $channelService->getById($id);
+
+        $this->view->payload = $channelService->toArray($channel);
+        $this->getResponse()
+            ->setHttpResponseCode(200);
+
+    }
+
+
+    private function channelDataToArray($channel)
+    {
+
+        $songService = new \WJB\Service\Song();
+        $current = array(
+            'progress' => null,
+            'song' => null
+        );
+
+        $states = $channel->getStates();
+        foreach ($states as $state)
+        {
+            switch ($state->getState())
+            {
+                case "progress":
+                    break;
+                case "current_song":
+                    $song = $songService->getById($state->getValue());
+                    $artist = $song->getArtist();
+                    //$album = $song->getAlbum;
+                    $songArray = array(
+                        'song_id' => $song->getId(),
+                        'song_title' => $song->getTitle(),
+                        'song_duration' => $song->getDuration(),
+                        'artist_id' => $artist->getId(),
+                        'artist_name' => $artist->getName()
+                    );
+                    $current['song'] = $songArray;
+                    break;
+            }
+            if ($state->getState() == 'progress')
+                $current['progress'] = (double) $state->getValue();
+        }
+
+        return array(
+            'id' => $channel->getId(),
+            'name' => $channel->getName(),
+            'backend' => $channel->getBackend(),
+            'current' => $current
+        );
+
+    }
 
 }
 
