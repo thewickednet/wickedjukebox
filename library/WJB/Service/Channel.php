5f65da4630c449b5d1131d668dc7186a994213ef
Yves Thommes <ythommes@gmail.com>
2012-07-02 17:42:07 +0200

    we need a special method which transforms this into an so we can use it with our rest interface

--- library/WJB/Service/Channel.php
+++ library/WJB/Service/Channel.php
@@ -13,4 +13,41 @@ class Channel extends \WJB\Service {
     }
 
 
+    public function toArray($channel)
+    {
+
+        $songService = new \WJB\Service\Song();
+        $current = array();
+
+        $states = $channel->getStates();
+        foreach ($states as $state)
+        {
+            switch ($state->getState())
+            {
+                case "progress":
+                break;
+                case "current_song":
+                    $song = $songService->getById($state->getValue());
+                    $artist = $song->getArtist();
+                    //$album = $song->getAlbum;
+                    $current['song_id'] = $song->getId();
+                    $current['song_title'] = $song->getTitle();
+                    $current['song_duration'] = $song->getDuration();
+                    $current['artist_id'] = $artist->getId();
+                    $current['artist_name'] = $artist->getName();
+                break;
+            }
+            if ($state->getState() == 'progress')
+                $current['progress'] = (double) $state->getValue();
+        }
+
+        return array(
+            'id' => $channel->getId(),
+            'name' => $channel->getName(),
+            'backend' => $channel->getBackend(),
+            'current' => $current
+        );
+
+    }
+
 }
