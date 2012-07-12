7bcf0380825f46da7f14e847fd7eef67635d68c4
Yves Thommes <ythommes@gmail.com>
2012-07-12 08:07:30 +0200

    Country entity added

---
+++ library/WJB/Entity/Country.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace WJB\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Country
+ *
+ * @ORM\Table(name="country")
+ * @ORM\Entity(repositoryClass="WJB\Entity\Repository\CountryRepository")
+ */
+class Country
+{
+    /**
+     * @var string $countryCode
+     *
+     * @ORM\Column(name="country_code", type="string", length=16)
+     * @ORM\Id
+     */
+    private $countryCode;
+
+    /**
+     * @var string $countryName
+     *
+     * @ORM\Column(name="country_name", type="string", length=100)
+     */
+    private $countryName;
+
+
+    /**
+     * @return string
+     */
+    public function getId()
+    {
+        return $this->countryCode;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCountryCode()
+    {
+        return $this->countryCode;
+    }
+
+    /**
+     * @param string $countryName
+     */
+    public function setCountryName($countryName)
+    {
+        $this->countryName = $countryName;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCountryName()
+    {
+        return $this->countryName;
+    }
+}
