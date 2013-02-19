f02ac0e40fbdcd4dbf0adc9cf7a1c3177dad91d3
Michel Albert <michel@albert.lu>
2013-02-19 18:51:29 +0100

    PEP8 indentation fix

--- wickedjukebox/demon/players/__init__.py
+++ wickedjukebox/demon/players/__init__.py
@@ -1,17 +1,17 @@
 def create(modname, backend_params=None):
-   # parse parameters, and put them into a dictionary
-   params = {}
-   if backend_params is not None:
-      for param in backend_params.split(','):
-         key, value = param.split('=')
-         params[key.strip()] = value.strip()
+    # parse parameters, and put them into a dictionary
+    params = {}
+    if backend_params is not None:
+        for param in backend_params.split(','):
+            key, value = param.split('=')
+            params[key.strip()] = value.strip()
 
-   if modname in globals():
-      module = reload(globals()[modname])
-      modname.config( params )
-      return module
+    if modname in globals():
+        module = reload(globals()[modname])
+        modname.config(params)
+        return module
 
-   module = 'wickedjukebox.demon.players.%s' % modname
-   __import__(module)
-   globals()[modname].config(params)
-   return globals()[modname]
+    module = 'wickedjukebox.demon.players.%s' % modname
+    __import__(module)
+    globals()[modname].config(params)
+    return globals()[modname]
