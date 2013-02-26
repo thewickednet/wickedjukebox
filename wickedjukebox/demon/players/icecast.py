cdf13a90d51ad5259b983a8854bdf32a8929f6d0
Michel Albert <michel@albert.lu>
2013-02-26 18:36:19 +0100

    A bit more of internal data caching...
    just trying things out...

--- wickedjukebox/demon/players/icecast.py
+++ wickedjukebox/demon/players/icecast.py
@@ -35,7 +35,7 @@ class Player(object):
         Player.LOG.debug('Initialising with {0!r}, {1!r}'.format(channel_id,
             icy_conf))
         self.source_command = Queue()
-        dataq = Queue(1)
+        dataq = Queue(16)
         icy_commands = Queue(1)
         self.filereader = FileReader(self.source_command, dataq, icy_commands)
         self.provider = IceProvider(dataq, icy_commands, icy_conf)
