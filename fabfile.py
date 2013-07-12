9770c57afb44ce3f2c2b3cd90ac38a176fb83ef8
Michel Albert <michel@albert.lu>
2013-07-12 10:50:14 +0200

    FAB: "develop" task will prompt for configuration.

--- fabfile.py
+++ fabfile.py
@@ -1,4 +1,7 @@
+from ConfigParser import SafeConfigParser
+
 import fabric.api as fab
+import fabric.colors as clr
 
 
 @fab.task
@@ -7,6 +10,16 @@ def develop():
     fab.local('./env/bin/pip install -e .')
     fab.local('./env/bin/pip install alembic')
 
-    ipy = fab.prompt('Do you want ipython [Y/n]?', default='Y').strip().upper()
+    ipy = fab.prompt(clr.green('Do you want ipython [Y/n]?'),
+                     default='Y').strip().upper()
     if ipy == 'Y':
         fab.local('./env/bin/pip install ipython')
+
+    conf = SafeConfigParser()
+    conf.read('alembic.ini.dist')
+    conf.read('alembic.ini')
+    url = fab.prompt(clr.green('Please enter the database DSN:'),
+                     default=conf.get('alembic', 'sqlalchemy.url'))
+    conf.set('alembic', 'sqlalchemy.url', url)
+    with open('alembic.ini', 'w') as fptr:
+        conf.write(fptr)
