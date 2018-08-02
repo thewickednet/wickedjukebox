6c21bde65283bd941dfd89054a963481e7bec5d7
Michel Albert <michel@albert.lu>
2018-08-02 12:59:38 +0200

    Replace optparse with argparse

--- wickedjukebox/cli/run_channel.py
+++ wickedjukebox/cli/run_channel.py
@@ -4,6 +4,7 @@ Entry points, and helpers for the command line interface.
 import logging
 import signal
 import time
+from argparse import ArgumentParser
 
 from wickedjukebox import __version__, setup_logging
 from wickedjukebox.core.channel import Channel
@@ -36,7 +37,7 @@ class Application(object):
                 LOG.info("Starting channel %s", channel_name)
                 self.channel.startPlayback()
                 self.channel.run()
-            except Exception: 
+            except Exception:
                 LOG.exception('Unhandled exception, trying to restart channel')
                 LOG.info("Restarting channel %s", channel_name)
                 self.channel.close()
@@ -54,24 +55,23 @@ def main():
     """
     Parse command line options, bootstrap the app and run the channel
     """
-    from optparse import OptionParser
     setup_logging()
 
     LOG.info(" Wicked Jukebox %s", __version__.center(79, '#'))
 
-    parser = OptionParser()
-    parser.add_option("-c", "--channel", dest="channel_name",
-                      help="runs the channel named CHANNEL_NAME",
-                      metavar="CHANNEL_NAME")
-    parser.add_option("-v", action="count", dest="verbosity",
-                      help=("Application verbosity. Can be repeated up to 5 "
-                            "times, each time increasing verbosity). If not "
-                            "set, the logging configuration file will take "
-                            "precedence."))
+    parser = ArgumentParser()
+    parser.add_argument("-c", "--channel", dest="channel_name",
+                        help="runs the channel named CHANNEL_NAME",
+                        metavar="CHANNEL_NAME")
+    parser.add_argument("-v", action="count", dest="verbosity",
+                        help=("Application verbosity. Can be repeated up to 5 "
+                              "times, each time increasing verbosity). If not "
+                              "set, the logging configuration file will take "
+                              "precedence."))
 
-    options, _ = parser.parse_args()
+    args = parser.parse_args()
 
-    if not options.channel_name:
+    if not args.channel_name:
         parser.error("CHANNEL_NAME is required!")
 
     verbosity_map = {
@@ -81,11 +81,11 @@ def main():
         2: logging.ERROR,
         1: logging.CRITICAL,
     }
-    if options.verbosity and options.verbosity in verbosity_map:
-        LOG.setLevel(verbosity_map[options.verbosity])
-    elif options.verbosity and options.verbosity > len(verbosity_map):
+    if args.verbosity and args.verbosity in verbosity_map:
+        LOG.setLevel(verbosity_map[args.verbosity])
+    elif args.verbosity and args.verbosity > len(verbosity_map):
         LOG.setLevel(logging.DEBUG)
 
     app = Application()
     signal.signal(signal.SIGINT, app.handle_sigint)
-    app.run_channel(options.channel_name)
+    app.run_channel(args.channel_name)
