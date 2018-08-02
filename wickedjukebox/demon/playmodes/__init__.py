b74ee4918a13c7a7b5a3c67abab0df721de7b329
Michel Albert <michel@albert.lu>
2018-08-02 23:38:34 +0200

    linting: indentation

--- wickedjukebox/demon/playmodes/__init__.py
+++ wickedjukebox/demon/playmodes/__init__.py
@@ -1,7 +1,7 @@
 def create(modname):
-   if modname in globals():
-      return reload(globals()[modname])
+    if modname in globals():
+        return reload(globals()[modname])
 
-   module = 'wickedjukebox.demon.playmodes.%s' % modname
-   __import__(module)
-   return globals()[modname]
+    module = 'wickedjukebox.demon.playmodes.%s' % modname
+    __import__(module)
+    return globals()[modname]
