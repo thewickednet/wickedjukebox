8ad9c6587f0d1bb63234d8a486c888928ea4290a
Michel Albert <michel@albert.lu>
2018-08-02 23:35:23 +0200

    linting: autopep8 & isort

--- wickedjukebox/demon/playmodes/queue_skeleton.py
+++ wickedjukebox/demon/playmodes/queue_skeleton.py
@@ -1,3 +1,12 @@
+# pylint: disable=invalid-name
+#
+# TODO This requires some names to be camel-case. This should be removed in the
+# future, but then the "player" API must change as well.
+
+# pylint: disable=unused-argument
+#
+# This is a "skeleton" file with empty implementations. Arguments are always
+# unused.
 """
 This is a no-op playmode used as blueprint for new playmodes.
 Simply complete the methods in this module and make them return the right type
@@ -10,120 +19,127 @@ and you should be fine.
 ## from wickedjukebox.demon.model import create_session, Song
 ## from wickedjukebox.demon.model import create_session, QueueItem
 
+
 def enqueue(songID, userID, channelID):
-   """
-   Enqueues a song onto a queue of a given channel
+    """
+    Enqueues a song onto a queue of a given channel
 
-   @type  songID: int
-   @param songID: The id of the song to be enqueued
+    @type  songID: int
+    @param songID: The id of the song to be enqueued
 
-   @type  channelID: int
-   @param channelID: The id of the channel
+    @type  channelID: int
+    @param channelID: The id of the channel
 
-   @type  userID: int
-   @param userID: The user who added the queue action
-   """
+    @type  userID: int
+    @param userID: The user who added the queue action
+    """
 
-   pass
+    pass
 
-def dequeue( channelID ):
-   """
-   Return the next song from the queue
 
-   @type  channelID: int
-   @param channelID: The id of the channel
+def dequeue(channelID):
+    """
+    Return the next song from the queue
 
-   @rtype:  Song
-   @return: This should return a valid "Song" instance, which points to the next song
-            on the queue.
-            If nothing was on the queue, or no song could be determnined, then return
-            "None", so the system can fall back to random mode.
-   """
+    @type  channelID: int
+    @param channelID: The id of the channel
 
-   return None
+    @rtype:  Song
+    @return: This should return a valid "Song" instance, which points to the next song
+             on the queue.
+             If nothing was on the queue, or no song could be determnined, then return
+             "None", so the system can fall back to random mode.
+    """
 
-def moveup( channelID, qid, delta ):
-   """
-   Move a song upwards in the queue by <delta> steps
-   (meaning it will be played earlier).
+    return None
 
-   @type channelID: int
-   @param channelID: The channel ID
 
-   @type  delta: int
-   @param delta: How many steps the song is move up in the queue
+def moveup(channelID, qid, delta):
+    """
+    Move a song upwards in the queue by <delta> steps
+    (meaning it will be played earlier).
 
-   @type  qid: int
-   @param qid: The database ID of the queue item (*not* the song!)
-   """
-   pass
+    @type channelID: int
+    @param channelID: The channel ID
 
-def movedown( channelID, qid, delta):
-   """
-   Move a song downwards in the queue by <delta> steps
-   (meaning it will be played later).
+    @type  delta: int
+    @param delta: How many steps the song is move up in the queue
 
-   @type channelID: int
-   @param channelID: The channel ID
+    @type  qid: int
+    @param qid: The database ID of the queue item (*not* the song!)
+    """
+    pass
 
-   @type  delta: int
-   @param delta: How many steps the song is move down in the queue
 
-   @type  qid: int
-   @param qid: The database ID of the queue item (*not* the song!)
-   """
-   pass
+def movedown(channelID, qid, delta):
+    """
+    Move a song downwards in the queue by <delta> steps
+    (meaning it will be played later).
 
-def movetop( channelID, qid ):
-   """
-   Move a song to the top of the queue (meaning it will be played next)
+    @type channelID: int
+    @param channelID: The channel ID
 
-   @type channelID: int
-   @param channelID: The channel ID
+    @type  delta: int
+    @param delta: How many steps the song is move down in the queue
 
-   @type  qid: int
-   @param qid: The database ID of the queue item (*not* the song!)
-   """
-   pass
+    @type  qid: int
+    @param qid: The database ID of the queue item (*not* the song!)
+    """
+    pass
 
-def movebottom( channelID, qid ):
-   """
-   Move a song to the bottom of the queue (meaning it will be played last)
 
-   @type channelID: int
-   @param channelID: The channel ID
+def movetop(channelID, qid):
+    """
+    Move a song to the top of the queue (meaning it will be played next)
 
-   @type  qid: int
-   @param qid: The database ID of the queue item (*not* the song!)
-   """
-   pass
+    @type channelID: int
+    @param channelID: The channel ID
 
-def list( channelID ):
-   """
-   Returns an ordered list of the items on the queue including position.
+    @type  qid: int
+    @param qid: The database ID of the queue item (*not* the song!)
+    """
+    pass
 
-   Example return value:
 
-       [{
-         'position': 1,
-         'song': {
-               'id':       123,
-               'title':    'supercool song'
-               'duration': 310
-            },
-         'album': {
-               'id':   234,
-               'name': 'My Album'
-            },
-         'artist': {
-               'id':   345,
-               'name': 'Wildecker Herzbuben'
-            }
-       }]
+def movebottom(channelID, qid):
+    """
+    Move a song to the bottom of the queue (meaning it will be played last)
 
-   @type channelID: int
-   @param channelID: The channel ID
+    @type channelID: int
+    @param channelID: The channel ID
 
-   @rtype: dict
-   """
-   pass
+    @type  qid: int
+    @param qid: The database ID of the queue item (*not* the song!)
+    """
+    pass
+
+
+def list(channelID):
+    """
+    Returns an ordered list of the items on the queue including position.
+
+    Example return value:
+
+        [{
+          'position': 1,
+          'song': {
+                'id':       123,
+                'title':    'supercool song'
+                'duration': 310
+             },
+          'album': {
+                'id':   234,
+                'name': 'My Album'
+             },
+          'artist': {
+                'id':   345,
+                'name': 'Wildecker Herzbuben'
+             }
+        }]
+
+    @type channelID: int
+    @param channelID: The channel ID
+
+    @rtype: dict
+    """
+    pass
