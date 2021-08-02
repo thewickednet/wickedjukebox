a28a930aa87e998d9bf0e3ef69002f5389b85aeb
Michel Albert <michel@albert.lu>
2021-08-02 20:43:38 +0200

    Refactor "scanner" for testing

--- wickedjukebox/scanner.py
+++ wickedjukebox/scanner.py
@@ -10,20 +10,27 @@ store the metadata in the jukebox database
 import logging
 import sys
 from os import listdir, path, walk
+from sys import stdout
+from typing import TextIO
+from progress.bar import ChargingBar
 
 from sqlalchemy.sql import select
 from wickedjukebox.demon.dbmodel import Session, Setting, Song, songTable
 
 LOG = logging.getLogger(__name__)
 
-EXTS = Setting.get("recognizedTypes", "").split(" ")
 
 
-def is_valid_audio_file(filename):
-    return filename.endswith(EXTS[0])
+def is_valid_audio_file(filename: str) -> bool:
+    value = Setting.get(None, "recognizedTypes", default="").strip()
+    if not value:
+        return False
+    exts = value.split(" ")
+    return filename.endswith(exts[0])
 
 
-def process(localpath):
+def process(localpath: str) -> None:
+    exts = Setting.get(None, "recognizedTypes", default="").split(" ")
 
     if not localpath:
         LOG.warning("Skipping undefined filename!")
@@ -33,7 +40,7 @@ def process(localpath):
 
     if is_valid_audio_file(localpath):
         try:
-            song = session.query(Song).filter_by(localpath=localpath).first()
+            song = Song.by_filename(session, localpath)
             if not song:
                 song = Song(localpath, None, None)
             song.scan_from_file(localpath, sys.getfilesystemencoding())
@@ -45,7 +52,7 @@ def process(localpath):
             LOG.error("Key Error: %s", exc)
     else:
         LOG.debug("%r is not a valid audio-file "
-                  "(only scanning extensions %r)", localpath, EXTS)
+                  "(only scanning extensions %r)", localpath, exts)
 
     session.commit()
     session.close()
@@ -65,8 +72,47 @@ def do_housekeeping():
         except UnicodeEncodeError as exc:
             LOG.error("Unable to decode %r (%s)", row[0], exc)
 
+def count_files(folder: str, stream: TextIO = stdout) -> None:
+    spinner_chars = r"/-\|"
+    LOG.info("Scanning %r", folder)
+    print("Scanning %r" % folder)
+    spinner_position = 0
+    stream.write("Counting... /")
+    count_total = 0
+    for _, _, files in walk(folder):
+        for file in files:
+            if not is_valid_audio_file(file):
+                continue
+            spinner_position = (spinner_position + 1) % len(spinner_chars)
+            stream.write("\b%s" % spinner_chars[spinner_position])
+            stream.flush()
+        count_total += len(files)
+    stream.write("\b ")
+    stream.flush()
+    stream.write("\n%d files to examine\n" % count_total)
+    process_recursive(folder, count_total, stream)
 
-def scan(top, subfolder=""):
+
+def process_recursive(folder: str, total_files: int, stream: TextIO = stdout) -> None:
+    pbar = ChargingBar("Scanning...", max=total_files)
+    count_scanned = 0
+    count_processed = 0
+    for root, _, files in walk(folder):
+        for file in files:
+            if not is_valid_audio_file(file):
+                continue
+            try:
+                process(path.join(root, file))
+                count_scanned += 1
+                count_processed += 1
+            except TypeError as exc:
+                LOG.error('Unable to scan %s (%s)',
+                            path.join(root, file), exc)
+            pbar.next()
+    pbar.finish()
+    stream.write("\n")
+
+def scan(top: str, subfolder: str="") -> None:
     """
     Scans a folder rootet at <top> for audio files. It will scan the supfolder
     named in ``subfolder``. The "*" character can be used for globbing.
@@ -74,51 +120,6 @@ def scan(top, subfolder=""):
     @param top: The root folder to scan
     @param subfolder: The subfolder to scan.
     """
-    from sys import stdout
-    from blessings import Terminal
-    from progress.bar import ChargingBar
-
-    spinner_chars = r"/-\|"
-
-    def scan_folder(folder):
-        LOG.info("Scanning %r", folder)
-        print("Scanning %r" % folder)
-        spinner_position = 0
-        stdout.write("Counting... /")
-        count_total = 0
-        for root, _, files in walk(folder):
-            for file in files:
-                if not any([file.endswith(_) for _ in EXTS]):
-                    continue
-                spinner_position = (spinner_position + 1) % len(spinner_chars)
-                stdout.write("\b%s" % spinner_chars[spinner_position])
-                stdout.flush()
-            count_total += len(files)
-        stdout.write("\b ")
-        stdout.flush()
-        stdout.write("\n%d files to examine\n" % count_total)
-
-        pbar = ChargingBar("Scanning...", max=count_total)
-
-        count_scanned = 0
-        count_processed = 0
-        completed_ratio = 0.0
-        for root, _, files in walk(folder):
-            for file in files:
-                if not any([file.endswith(_) for _ in EXTS]):
-                    continue
-                try:
-                    process(path.join(root, file))
-                    count_scanned += 1
-                    count_processed += 1
-                    completed_ratio = float(
-                        count_processed) / float(count_total)
-                except TypeError as exc:
-                    LOG.error('Unable to scan %s (%s)',
-                              path.join(root, file), exc)
-                pbar.next()
-        pbar.finish()
-        stdout.write("\n")
 
     glob = subfolder.endswith('*')
 
@@ -126,6 +127,6 @@ def scan(top, subfolder=""):
         subfolder = subfolder[0:-1]
         candidates = [_ for _ in listdir(top) if _.startswith(subfolder)]
         for candidate in candidates:
-            scan_folder(path.join(top, candidate))
+            count_files(path.join(top, candidate))
     else:
-        scan_folder(path.join(top, subfolder))
+        count_files(path.join(top, subfolder))
