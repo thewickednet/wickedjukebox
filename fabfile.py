a8c45f5b0f5f26320b6a00e268e7b1f49ad0fad1
Michel Albert <michel@albert.lu>
2021-08-05 13:37:40 +0200

    Set DB-port automatically on "fab develop"

--- fabfile.py
+++ fabfile.py
@@ -1,36 +1,57 @@
-from pathlib import Path
+import json
 from os import getuid
 from os.path import abspath
+from pathlib import Path
 from tempfile import NamedTemporaryFile
+from typing import Dict
 
 from invoke import task
 
 
-def reconfigure(ctx, template: Path, target: Path) -> None:
+def get_db_port(json_doc: str) -> int:
+
+    data = json.loads(json_doc)
+    ports = data[0]["NetworkSettings"]["Ports"]["3306/tcp"]
+    port_numbers = {int(info["HostPort"]) for info in ports}
+    assert len(port_numbers) == 1
+    return port_numbers.pop()
+
+
+def reconfigure(
+    ctx, template: Path, target: Path, variables: Dict[str, str]
+) -> None:
     if not template.is_file():
-        raise ValueError('%s should be a regular file!' % template)
+        raise ValueError("%s should be a regular file!" % template)
     if target.exists() and not target.is_file():
-        raise ValueError('%s should be a regular file!' % target)
+        raise ValueError("%s should be a regular file!" % target)
 
     if target.exists():
         # Instead of using the application template, we will use the existing
         # file to edit
         template = target
 
+    template_str = template.read_text()
+
+    for key, value in variables.items():
+        template_str = template_str.replace("{{%s}}" % key, value)
+
     with NamedTemporaryFile() as tmpfile:
-        ctx.run('cat %s > %s' % (template, tmpfile.name))
-        ctx.run('$EDITOR %s' % tmpfile.name, pty=True, replace_env=False)
-        diff = ctx.run('diff %s %s' % (tmpfile.name, template), warn=True,
-                   hide=True)
+        tmpfile.write(template_str.encode("utf8"))
+        tmpfile.flush()
+
+        ctx.run("$EDITOR %s" % tmpfile.name, pty=True, replace_env=False)
+        diff = ctx.run(
+            "diff %s %s" % (tmpfile.name, template), warn=True, hide=True
+        )
 
         if diff.exited == 0:
             # no changes to file. we can return with no-op
-            print('No changes made. Keeping old file.')
+            print("No changes made. Keeping old file.")
             return
 
-        print('Applying changes %s -> %s' % (template, target))
-        ctx.run('mkdir -p %s' % target.parent)
-        ctx.run('cp -v %s %s' % (tmpfile.name, target))
+        print("Applying changes %s -> %s" % (template, target))
+        ctx.run("mkdir -p %s" % target.parent)
+        ctx.run("cp -v %s %s" % (tmpfile.name, target))
 
 
 @task
@@ -38,46 +59,53 @@ def develop(ctx):
     ctx.run("[ -d env ] || python3 -m venv env")
     ctx.run("./env/bin/pip install -U pip")
     ctx.run("./env/bin/pip install -e .[dev,test]")
-    ctx.run('./db_container.sh', warn=True, pty=True)
-    print('The following config-file needs the MySQL port you see above!')
-    input('Press ENTER to continue...')
+    print("Running DB container...")
+    ctx.run("./db_container.sh", warn=True, pty=True)
+    output = ctx.run("docker inspect jukeboxdb", hide="both")
+    db_port = get_db_port(output.stdout)
+    print("The following config-file needs the MySQL port you see above!")
+    input("Press ENTER to continue...")
     reconfigure(
         ctx,
-        Path('alembic.ini.dist'),
-        Path('alembic.ini'))
+        Path("alembic.ini.dist"),
+        Path("alembic.ini"),
+        variables={"db_port": str(db_port)},
+    )
     reconfigure(
         ctx,
-        Path('config.ini.dist'),
-        Path('.wicked/wickedjukebox/config.ini'))
-    print('Running DB container...')
-    ctx.run('./env/bin/alembic upgrade head')
+        Path("config.ini.dist"),
+        Path(".wicked/wickedjukebox/config.ini"),
+        variables={"db_port": str(db_port)},
+    )
+    ctx.run("./env/bin/alembic upgrade head")
 
 
 @task
 def test(ctx, autorun=False, cover=False, lf=False):
     from configparser import ConfigParser
+
     cfg = ConfigParser()
     cfg.read(".wicked/wickedjukebox/config.ini")
-    dsn = cfg.get('database', 'dsn')
+    dsn = cfg.get("database", "dsn")
 
     if autorun:
         base_cmd = 'git ls-files | entr -c sh -c "%s"'
     else:
-        base_cmd = '%s'
+        base_cmd = "%s"
 
-    runner_cmd = ['./env/bin/pytest --sqlalchemy-connect-url=%s' % dsn]
+    runner_cmd = ["./env/bin/pytest --sqlalchemy-connect-url=%s" % dsn]
 
     if lf:
         runner_cmd.append("--lf")
 
     if cover:
         runner_cmd.append(
-            '--cov-report=term-missing '
-            '--cov-report=xml:coverage.xml '
-            '--cov wickedjukebox '
+            "--cov-report=term-missing "
+            "--cov-report=xml:coverage.xml "
+            "--cov wickedjukebox "
         )
 
-    runner_cmd = ' '.join(runner_cmd)
+    runner_cmd = " ".join(runner_cmd)
     ctx.run(base_cmd % runner_cmd, pty=True, replace_env=False)
 
 
