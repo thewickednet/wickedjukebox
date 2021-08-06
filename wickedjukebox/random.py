ac589596dcde657f287c9472b76bdb8b1a42d19f
Michel Albert <michel@albert.lu>
2021-08-06 22:27:27 +0200

    Implement simple/naive random mode

--- wickedjukebox/random.py
+++ wickedjukebox/random.py
@@ -1,8 +1,10 @@
-from abc import ABC, abstractmethod
 import logging
-from wickedjukebox.logutil import qualname
+from abc import ABC, abstractmethod
+from pathlib import Path
+from random import choice
 
 from wickedjukebox.adt import Song
+from wickedjukebox.logutil import qualname
 
 
 class AbstractRandom(ABC):
@@ -19,5 +21,37 @@ class AbstractRandom(ABC):
 
 
 class NullRandom(AbstractRandom):
+    """
+    Dummy implementation resulting in no-ops.
+
+    This always returns a song with an ampty filename. This may very well lead
+    to errors downstream. This implementation is primarily intended for use in a
+    test-harness.
+    """
+
     def pick(self) -> Song:
         return Song("", "", "", "")
+
+
+class AllFilesRandom(AbstractRandom):
+    """
+    An implementation of random picking based on a file-tree.
+
+    It builds possible file-names recursively from a root and picks one file at
+    random.
+    """
+
+    def __init__(self, root: str) -> None:
+        super().__init__()
+        self.root = root
+
+    def pick(self) -> Song:
+        pth = Path(self.root)
+        pick = choice(list(pth.glob("**/*.mp3")))
+        output = Song("", "", "", str(pick.absolute()))
+        self._log.debug(
+            "Picked %r as random file from all files in %r",
+            output,
+            pth.absolute(),
+        )
+        return output
