f04668f01b42c4194332d8d29db004926b8d9741
Michel Albert <michel@albert.lu>
2021-08-09 17:06:26 +0200

    Make run-channel use new architecture (WIP)

--- wickedjukebox/cli/run_channel.py
+++ wickedjukebox/cli/run_channel.py
@@ -2,81 +2,20 @@
 Entry points, and helpers for the command line interface.
 """
 import logging
-import signal
-import time
-from argparse import ArgumentParser
+from argparse import ArgumentParser, Namespace
+from os import getenv
+from pathlib import Path
 
 from wickedjukebox import __version__, setup_logging
-from wickedjukebox.core.channel import Channel
+from wickedjukebox.channel import Channel
+from wickedjukebox.jingle import FileBasedJingles
+from wickedjukebox.player import MpdPlayer, PathMap
+from wickedjukebox.random import AllFilesRandom
 
 LOG = logging.getLogger(__name__)
 
 
-class Application(object):
-
-    def __init__(self):
-        self.channel = None
-        self.keep_running = True
-
-    def handle_sigint(self, signal_num, frame):  # pylint: disable=unused-argument
-        """
-        Try to exit cleanly on SIGINT.
-        """
-        LOG.info("SIGINT caught")
-        self.channel.close()
-        self.keep_running = False
-
-    def run_channel(self, channel_name):
-        """
-        Starts a channel
-        """
-
-        while self.keep_running:
-            self.channel = Channel(channel_name)
-            try:
-                LOG.info("Starting channel %s", channel_name)
-                self.channel.startPlayback()
-                self.channel.run()
-            except Exception:  # pylint: disable=broad-except
-                # We allow "broad-except" here. This is a
-                # "shit-has-hit-the-fan" code. Trying to restart the channel
-                # might resolve the issue.
-                LOG.exception('Unhandled exception, trying to restart channel')
-                LOG.info("Restarting channel %s", channel_name)
-                self.channel.close()
-                self.channel = None
-                self.channel = Channel(channel_name)
-
-            if self.channel:
-                LOG.info("Closing channel")
-                self.channel.close()
-
-            time.sleep(1)
-
-
-def main():
-    """
-    Parse command line options, bootstrap the app and run the channel
-    """
-    setup_logging()
-
-    LOG.info(" Wicked Jukebox %s", __version__.center(79, '#'))
-
-    parser = ArgumentParser()
-    parser.add_argument("-c", "--channel", dest="channel_name",
-                        help="runs the channel named CHANNEL_NAME",
-                        metavar="CHANNEL_NAME")
-    parser.add_argument("-v", action="count", dest="verbosity",
-                        help=("Application verbosity. Can be repeated up to 5 "
-                              "times, each time increasing verbosity). If not "
-                              "set, the logging configuration file will take "
-                              "precedence."))
-
-    args = parser.parse_args()
-
-    if not args.channel_name:
-        parser.error("CHANNEL_NAME is required!")
-
+def set_log_verbosity(verbosity: int) -> None:
     verbosity_map = {
         5: logging.DEBUG,
         4: logging.INFO,
@@ -84,11 +23,62 @@ def main():
         2: logging.ERROR,
         1: logging.CRITICAL,
     }
-    if args.verbosity and args.verbosity in verbosity_map:
-        LOG.setLevel(verbosity_map[args.verbosity])
-    elif args.verbosity and args.verbosity > len(verbosity_map):
-        LOG.setLevel(logging.DEBUG)
+    verbosity = verbosity_map.get(verbosity, logging.DEBUG)
+    logging.getLogger().setLevel(verbosity)
 
-    app = Application()
-    signal.signal(signal.SIGINT, app.handle_sigint)
-    app.run_channel(args.channel_name)
+
+def parse_args() -> Namespace:
+    parser = ArgumentParser()
+    parser.add_argument(
+        "-c",
+        "--channel",
+        dest="channel_name",
+        help="runs the channel named CHANNEL_NAME",
+        metavar="CHANNEL_NAME",
+    )
+    parser.add_argument(
+        "-v",
+        action="count",
+        dest="verbosity",
+        help=(
+            "Application verbosity. Can be repeated up to 5 "
+            "times, each time increasing verbosity). If not "
+            "set, the logging configuration file will take "
+            "precedence."
+        ),
+    )
+    args = parser.parse_args()
+    return args
+
+
+def make_channel(channel_name: str) -> Channel:
+    # TODO: Read these value from the channel-config in the database
+    mpd_host = getenv("MPD_HOST", "127.0.0.1")
+    mpd_port = int(getenv("MPD_PORT", 6600))
+    channel = Channel(
+        channel_name,
+        random=AllFilesRandom("mp3s/Tagged"),
+        jingle=FileBasedJingles("mp3s/Jingles"),
+        player=MpdPlayer(
+            host=mpd_host,
+            port=mpd_port,
+            path_map=PathMap(
+                Path("/home/exhuma/work/wickedjukebox/mp3s"),
+                Path("/var/lib/mp3s/music"),
+            ),
+        ),
+        tick_interval_s=10,
+    )
+    return channel
+
+
+def main():
+    """
+    Parse command line options, bootstrap the app and run the channel
+    """
+    setup_logging()
+    LOG.info(" Wicked Jukebox %s", __version__.center(79, "#"))
+    args = parse_args()
+    set_log_verbosity(args.verbosity)
+    channel = make_channel(args.channel_name)
+    channel.run()
