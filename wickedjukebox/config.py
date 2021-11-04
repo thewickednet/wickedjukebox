1fd6e1e7e1bdd3ebba8a971ad9bd475321ae69df
Michel Albert <michel@albert.lu>
2021-11-04 08:47:45 +0100

    Add "scope" to config-values

--- wickedjukebox/config.py
+++ wickedjukebox/config.py
@@ -2,14 +2,27 @@
 This module contains helpers for application configuration
 """
 
+import logging
 from configparser import ConfigParser
 from enum import Enum
-from typing import Any, Callable, Dict, List, Type, TypeVar
+from typing import Any, Callable, Dict, List, NamedTuple, Type, TypeVar
 
 from config_resolver import get_config
 
 T = TypeVar("T", bound=Type[Any])
 NO_DEFAULT = object()
+LOG = logging.getLogger(__name__)
+
+
+class ConfigScope(Enum):
+    CORE = "core"
+    CHANNEL = "channel"
+
+
+class ConfigOption(NamedTuple):
+    scope: ConfigScope
+    section: str
+    subsection: str
 
 
 def parse_param_string(value: str) -> Dict[str, str]:
@@ -19,7 +32,7 @@ def parse_param_string(value: str) -> Dict[str, str]:
     >>> parse_param_string("a=2, b=20, c=hello")
     {'a': '2', 'b': '20', 'c': 'hello'}
     """
-    output = {}
+    output: Dict[str, str] = {}
     if value:
         for param in value.split(","):
             key, _, value = param.partition("=")
@@ -62,24 +75,37 @@ class ConfigKeys(Enum):
     A well-defined list of config-values in the config-file with the sections
     they are expected to be in.
     """
-    CHANNEL_CYCLE = ("channel", "channel_cycle")
-    JINGLES_FOLDER = ("channel", "jingles_folder")
-    JINGLES_INTERVAL = ("channel", "jingles_interval")
-    LASTFM_API_KEY = ("core", "lastfm_api_key")
-    MAX_CREDITS = ("core", "max_credits")
-    MAX_RANDOM_DURATION = ("core", "max_random_duration")
-    PROOFOFLIFE_TIMEOUT = ("core", "proofoflife_timeout")
-    QUEUE_MODEL = ("core", "queue_model")
-    RANDOM_MODEL = ("core", "random_model")
-    SCORING_LASTPLAYED = ("core", "scoring_lastplayed")
-    SCORING_NEVERPLAYED = ("core", "scoring_neverplayed")
-    SCORING_RANDOMNESS = ("core", "scoring_randomness")
-    SCORING_SONGAGE = ("core", "scoring_songage")
-    SCORING_USERRATING = ("core", "scoring_userrating")
-    SYS_UTCTIME = ("core", "sys_utctime")
-    RECENCY_THRESHOLD = ("core", "recency_threshold")
-    PLAYER_SETTINGS = ("core", "player_settings")
-    PLAYER = ("core", "player")
+
+    CHANNEL_CYCLE = ConfigOption(ConfigScope.CHANNEL, "channel_cycle", "")
+    JINGLES_FOLDER = ConfigOption(ConfigScope.CHANNEL, "jingles_folder", "")
+    JINGLES_INTERVAL = ConfigOption(ConfigScope.CHANNEL, "jingles_interval", "")
+    LASTFM_API_KEY = ConfigOption(ConfigScope.CORE, "lastfm_api_key", "")
+    MAX_CREDITS = ConfigOption(ConfigScope.CORE, "max_credits", "")
+    MAX_RANDOM_DURATION = ConfigOption(
+        ConfigScope.CORE, "max_random_duration", ""
+    )
+    PROOFOFLIFE_TIMEOUT = ConfigOption(
+        ConfigScope.CORE, "proofoflife_timeout", ""
+    )
+    QUEUE_MODEL = ConfigOption(ConfigScope.CORE, "queue_model", "")
+    RANDOM_MODEL = ConfigOption(ConfigScope.CORE, "random_model", "")
+    SCORING_LASTPLAYED = ConfigOption(
+        ConfigScope.CORE, "scoring_lastplayed", ""
+    )
+    SCORING_NEVERPLAYED = ConfigOption(
+        ConfigScope.CORE, "scoring_neverplayed", ""
+    )
+    SCORING_RANDOMNESS = ConfigOption(
+        ConfigScope.CORE, "scoring_randomness", ""
+    )
+    SCORING_SONGAGE = ConfigOption(ConfigScope.CORE, "scoring_songage", "")
+    SCORING_USERRATING = ConfigOption(
+        ConfigScope.CORE, "scoring_userrating", ""
+    )
+    SYS_UTCTIME = ConfigOption(ConfigScope.CORE, "sys_utctime", "")
+    RECENCY_THRESHOLD = ConfigOption(ConfigScope.CORE, "recency_threshold", "")
+    PLAYER_SETTINGS = ConfigOption(ConfigScope.CHANNEL, "player", "settings")
+    PLAYER = ConfigOption(ConfigScope.CHANNEL, "player", "backend")
 
 
 class Config:
@@ -91,9 +117,11 @@ class Config:
         converter: Callable[[str], T] = lambda x: x,
     ) -> T:
         config = load_config()
-        section, option = key.value
-        if channel:
-            section = f"channel:{channel}"
+        scope, section, option = key.value
+        if scope == ConfigScope.CHANNEL:
+            section = f"channel:{channel}:{section}"
+        else:
+            section = "core"
         if fallback is NO_DEFAULT:
             value = config.get(section, option)
         else:
