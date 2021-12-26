6e88b56eafe25927ae3ef65a507b90bed12bed64
Michel Albert <michel@albert.lu>
2021-12-26 12:31:45 +0100

    Refactor queries for the "smart" find process

--- wickedjukebox/core/smartfind.py
+++ wickedjukebox/core/smartfind.py
@@ -1,27 +1,34 @@
 """
-This module contains some of the more complex SQL queries to keep the rest of
-the code a bit more readable
+This module contains an entry point to run a "smart" query to find the next best
+song taking channel statistics into account.
 """
-# pylint: disable=no-member
+import logging
+from typing import TYPE_CHECKING, Any, Optional
 
-
-from typing import Any, TYPE_CHECKING
+import sqlalchemy.orm as orm
 from sqlalchemy.orm.query import Query
-from sqlalchemy.sql import select
-from sqlalchemy.sql.expression import text, alias, join, func, and_
+from sqlalchemy.sql import func, select
+from sqlalchemy.sql.elements import not_
+from sqlalchemy.sql.expression import alias, and_, func, join, text
+
+from wickedjukebox.config import Config, ConfigKeys
 from wickedjukebox.model.database import (
-    Song,
-    channelSongs,
-    Artist,
     Album,
+    Artist,
+    Song,
+    User,
+    channelSongs,
     settingTable,
     songStandingTable,
+    songTable,
     usersTable,
 )
+from wickedjukebox.smartplaylist.dbbridge import parse_dynamic_playlists
 
 if TYPE_CHECKING:
     from typing import Tuple
 
+LOG = logging.getLogger(__name__)
 
 #: A song gains the maximum "last-played" score boost after this many seconds
 LAST_PLAYED_CUTOFF = 7 * 24 * 60 * 60
@@ -127,13 +134,13 @@ def smart_random_no_users(
                     alias(Song, "s"),  # type: ignore
                     alias(channelSongs, "c"),
                     isouter=True,
-                    onclause=text("c.song_id = s.id")
+                    onclause=text("c.song_id = s.id"),
                 ),
                 alias(Artist, "a"),  # type: ignore
-                onclause=text("s.artist_id = s.artist_id")
+                onclause=text("s.artist_id = s.artist_id"),
             ),
             alias(Album, "b"),  # type: ignore
-            onclause=text("s.album_id = b.id")
+            onclause=text("s.album_id = b.id"),
         )
     )
 
@@ -231,3 +238,115 @@ def smart_random_with_users(
     query = query.limit(10)  # type: ignore
     query = query.offset(0)  # type: ignore
     return query  # type: ignore
+
+
+def find_song(session: orm.Session, channel_name: str) -> Optional[Song]:
+    # pylint: disable=too-many-statements, too-many-locals
+    #
+    # This may be difficult to refactorl The high count of statements and
+    # locals comes partly from constructing the select-query and fetching
+    # settings. Ignoring this warning for now.
+    """
+    determine a song that would be best to play next and return it
+    """
+
+    # setup song scoring coefficients
+    user_rating = Config.get(
+        ConfigKeys.SCORING_USERRATING,
+        4,
+        channel=channel_name,
+        converter=int,
+    )
+    last_played = Config.get(
+        ConfigKeys.SCORING_LASTPLAYED,
+        10,
+        channel=channel_name,
+        converter=int,
+    )
+    song_age = Config.get(
+        ConfigKeys.SCORING_SONGAGE,
+        1,
+        channel=channel_name,
+        converter=int,
+    )
+    never_played = Config.get(
+        ConfigKeys.SCORING_NEVERPLAYED,
+        4,
+        channel=channel_name,
+        converter=int,
+    )
+    randomness = Config.get(
+        ConfigKeys.SCORING_RANDOMNESS, 1, channel=channel_name, converter=int
+    )
+    max_random_duration = Config.get(
+        ConfigKeys.MAX_RANDOM_DURATION,
+        600,
+        channel=channel_name,
+        converter=int,
+    )
+    proofoflife_timeout = Config.get(
+        ConfigKeys.PROOFOFLIFE_TIMEOUT, 120, converter=int
+    )
+    is_mysql = Config.get(ConfigKeys.DSN, "").lower().startswith("mysql")
+
+    if is_mysql:
+
+        query = session.query(User.id).filter(
+            func.unix_timestamp(User.proof_of_listening) + proofoflife_timeout
+            > func.unix_timestamp(func.now())
+        )
+        if query.count() == 0:
+            # no users online
+            query = smart_random_no_users(
+                never_played,
+                last_played,
+                song_age,
+                randomness,
+                max_random_duration,
+            )
+        else:
+            query = smart_random_with_users(
+                never_played,
+                user_rating,
+                last_played,
+                proofoflife_timeout,
+                randomness,
+                song_age,
+                max_random_duration,
+            )
+    else:
+        raise Exception(
+            "SQLite support discontinued since revision 346. It may reappear in the future!"
+        )
+
+    query = query.where(not_(text("s.broken")))  # type: ignore
+
+    # TODO: Add dynamic playlist clauses
+    # TODO fix cross-product issue query = query.where(and_(*parse_dynamic_playlists()))
+    query = query.where(and_(*parse_dynamic_playlists()))
+
+    LOG.debug(query)
+    res = session.execute(query)
+    candidate = res.first()
+
+    if candidate is None:
+        return None
+
+    try:
+        if not candidate.score:
+            # no users are online!
+            session.close()
+            return None
+        out = (candidate.id, candidate.localpath, float(candidate.score))
+        LOG.info("Selected song (%d, %s) via smartget. Score was %4.3f", *out)
+        selected_song = (
+            session.query(Song).filter(songTable.c.id == out[0]).first()
+        )
+        session.close()
+        return selected_song
+    except IndexError:
+        LOG.warning(
+            "No song returned from query. Is the database empty?", exc_info=True
+        )
+        session.close()
+        return None
