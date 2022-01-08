387266d493421768e56f246253f15d6d941b5e9d
Michel Albert <michel@albert.lu>
2022-01-08 13:33:51 +0100

    Version bumped to 3.0.0a4

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ NAME = "wickedjukebox"
 
 setup(
     name=NAME,
-    version="3.0.0a3",
+    version="3.0.0a4",
     packages=find_packages(exclude=["test"]),
     entry_points={
         "console_scripts": [
