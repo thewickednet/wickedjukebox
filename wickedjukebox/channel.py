80fe521d8a76c0c87f8473228550fd96e2c74f86
Michel Albert <michel@albert.lu>
2022-08-07 09:52:41 +0200

    Cleanup exception handling

--- wickedjukebox/channel.py
+++ wickedjukebox/channel.py
@@ -165,8 +165,10 @@ class Channel:
         while self.keep_running:
             try:
                 self.tick()
-                sleep(self.tick_interval_s)
             except KeyboardInterrupt:
                 self._log.info("Caught SIGINT. Bye!")
                 self.keep_running = False
+            except Exception:  # pylint: disable=broad-except
+                self._log.exception("Unhandled exception")
+            sleep(self.tick_interval_s)
         self._log.info("Stopping channel %r", self.name)
