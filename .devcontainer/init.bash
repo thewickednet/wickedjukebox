e90de32da1bd3b6dcb4bfdc424119b1e675b6ee6
Michel Albert <michel@albert.lu>
2023-07-08 18:32:33 +0200

    Cleanup devcontainer setup

--- .devcontainer/init.bash
+++ .devcontainer/init.bash
@@ -2,6 +2,15 @@
 
 set -xe
 
+pip install -U pip
+pip install pipx
+pipx install fabric
+pipx install pre-commit
+
+
 [ -d env ] || python3 -m venv env
 ./env/bin/pip install -U pip
 ./env/bin/pip install -e ".[dev]"
+./env/bin/wjb migrate
+
+pre-commit install
